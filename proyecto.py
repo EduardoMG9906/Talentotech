c27097035489494cbf9c9bfc167967506649daf6
EduardoMontoya <70491259+EduardoMG9906@users.noreply.github.com>
2025-02-17 07:09:24 -0500

    Update proyecto.py
    .

---
+++ proyecto.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+from datetime import datetime
+
+st.set_page_config(
+  page_title= "😎Dashboard Interactivo",
+  layout="wide"
+)
+
+st.title(" 😎 Dashboard Interactivo con Streamlit")
+st.sidebar.title("Opciones de Navegacion")
